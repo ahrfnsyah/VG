b5a6f1c6655eb1ae77af7face74c1ef695a11f65
Clausensan <ahmadhuseinrifansyah@gmail.com>
2025-05-10 22:12:27 +0700

    commmmmit

--- vegenation/next.config.ts
+++ vegenation/next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true
-  ,srcDir: 'src',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
